50785e5c8d92c279ef8cc86cdb005e6081e77e9c
KuanLiang <kuanliang@wiseai.tech>
2026-03-24 17:27:55 +0800

    Use GitHub Releases zip for 2.1.2 binary target

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WiseAISDK",
-            path: "WiseAISDK.xcframework"
+            url: "https://github.com/WiseAI-Tech/WiseAISDK-iOS-SDK_SPM/releases/download/2.1.2/WiseAISDK.xcframework.zip",
+            checksum: "db02a1d52b08dda3a25bcf766041f23d061f32b2402640dbfdc74ae9a0e14a10"
         )
     ]
 )
